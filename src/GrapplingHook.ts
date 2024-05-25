323d859943f0109f543ad4bf5007b61f44fa49bf
Mirucow <115716637+Mirucow@users.noreply.github.com>
2024-05-25 19:06:31 +0900

    Add config to `GrapplingHook`

--- src/GrapplingHook.ts
+++ src/GrapplingHook.ts
@@ -6,8 +6,18 @@ import { procHacker } from "bdsx/prochacker";
 
 
 
+const Power = 0.5;
+const Height = {
+  Initial: 0.5,
+  Max: 3.5,
+  Multiplier: 0.17
+}
+
+
+
 class FishingHook extends Actor {
 }
+
 
 const FishingHook$getOwner = procHacker.js("?getOwner@FishingHook@@QEAAPEAVActor@@XZ", Actor, {this: FishingHook});
 const FishingHook$retrieve = procHacker.hooking("?retrieve@FishingHook@@QEAAHXZ", int32_t, {this: FishingHook})(onRetrieve);
@@ -24,13 +34,13 @@ function onRetrieve(this: FishingHook): number {
 
       const angle = Math.atan2(oPos.z - hPos.z, oPos.x - hPos.x);
 
-      const r = owner.distanceTo(this.getPosition()) / 2;
+      const r = owner.distanceTo(this.getPosition()) * Power;
 
       const x = r * Math.cos(angle);
-      const y = 0.5 + (hPos.y - oPos.y) / 6;
+      const y = Height.Initial + (hPos.y - oPos.y) * Height.Multiplier;
       const z = r * Math.sin(angle);
 
-      owner.knockback(owner, 0, x, z, r, y, 3.5);
+      owner.knockback(owner, 0, x, z, r, y, Height.Max);
     }
   }
 
